6e3a2d223acfbd3bcfffb9c844cb645136e75cb1
jgiron <jgiron@student.42.fr>
2021-09-22 15:25:41 +0200

    added command_not_found.hpp and protection of syscalls

---
+++ srcs/commands/command_not_found.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Joachim GIRON on 8/26/21.
+//
+
+#ifndef FT_IRC_COMMAND_NOT_FOUND_HPP
+#define FT_IRC_COMMAND_NOT_FOUND_HPP
+
+#include "command.hpp"
+
+class command_not_found : public command {
+public:
+	command_not_found(class client &c, class server &s) : command(c, s) {
+		name = "";
+		syntax = "";
+	};
+	void execute() {
+		this->reply_nbr(ERR_UNKNOWNCOMMAND);
+	}
+};
+
+#endif //FT_IRC_NICK_HPP
