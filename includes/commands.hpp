54354c69baa62a1510f624c15d5dcb9e4ff2f2e1
00101010 <00101010@00101010.fr>
2022-02-01 16:35:25 +0100

    cleaned commands.hpp

--- includes/commands.hpp
+++ includes/commands.hpp
@@ -17,35 +17,12 @@
 #include "quit.hpp"
 #include "command_not_found.hpp"
 
-
-class zbeub_command : public command {
-public:
-	zbeub_command(class client &c, class server &s) : command(c, s) {
-		name = "ZBEUB";
-		syntax = "{,<argn>;<argt>}";
-		add_block(ELEM, "argn");
-		add_block(REP, "");
-		add_block(CHAR, ",");
-		add_block(ELEM, "argn");
-		add_block(CHAR, ";");
-		add_block(ELEM, "argt");
-		add_block(REPE, "");
-	};
-	void execute() {
-	}
-};
-
-
 typedef command *creator(client &, server &);
 
 template <class U>
 command *f(client &c, server &s)
 {
 	return (new U(c, s));
-}
-
-command *nullf(client &, server &) {
-	return(NULL);
 }
 
 creator *get_command(std::string name)
@@ -61,14 +38,13 @@ creator *get_command(std::string name)
 		map["QUIT"] = &f<quit_command>;
 		map["VERSION"] = &f<version_command>;
 		map["TIME"] = &f<time_command>;
-		map["ZBEUB"] = &f<zbeub_command>;
 	}
 	try {
 		return (map.at(name));
 	}
-	catch (std::out_of_range e)	{
+	catch (std::out_of_range &e)	{
 		return (&f<command_not_found>);
 	}
 }
 
-#endif //FT_IRC_COMMANDS_HPP
+#endif //FT_IRC_COMMANDS_HPP.at(
