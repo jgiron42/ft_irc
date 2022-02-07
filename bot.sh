4fb573aaeb93dfe0a53c02746086235d06f94aa1
00101010 <00101010@00101010.fr>
2022-02-07 12:40:21 +0100

    j'ai fais un minibot

--- bot.sh
+++ bot.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
-
 HOST=localhost
 PORT=1111
 PASSWORD=
-
 mkfifo .irc_fifo
-
-(echo  "
-NICK bc
-USER a b c d
-EOF
-" && cat .irc_fifo) | nc $HOST $PORT | while read from command message
+(echo  "NICK bc\nPASS $PASSWORD\nUSER a b c d" && cat .irc_fifo) | nc $HOST $PORT | while read from command message
 do
-	if [ "$command" = "PRIVMSG" ]
-	then
-		echo -n "PRIVMSG $(echo $from | tr -d \: | cut -d! -f 1) :"
-		echo "$message" | tr -d \:  | bc
-	fi
+  [ "$command" = "PRIVMSG" ] && echo -n "PRIVMSG $(echo $from | tr -d \: | cut -d! -f 1) :" && echo "$message" | tr -d \:  | bc
 done > .irc_fifo 2>&1
-
 rm .irc_fifo
