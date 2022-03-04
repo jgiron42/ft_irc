8b17266815698ae3e4e8900322b030d43fb8be30
joachim <joachim@00101010.fr>
2022-03-04 09:27:00 +0100

    bot ok

--- bot.sh
+++ bot.sh
@@ -5,6 +5,6 @@ PASSWORD=lol
 mkfifo .irc_fifo 2>/dev/null
 (echo -n "NICK bc\r\nPASS $PASSWORD\r\nUSER bc bc bc bc\r\n" && cat .irc_fifo) | nc $HOST $PORT | while  read from command to message
 do
-	[ "$command" = "PRIVMSG" ] && (echo -n "PRIVMSG $(echo $from | tr -d \: | cut -d! -f 1) :" && echo -n "$message\n" | tr -d \: | tr -d '\r' | bc | tr -d '\n' ; echo '\r\n')
+	[ "$command" = "PRIVMSG" ] && (echo -n "PRIVMSG $(echo $from | tr -d \: | cut -d! -f 1) :" && echo -n "$message\n" | tr -d \: | tr -d '\r' | bc | tr -d '\n' ; echo '\r')
 done  2>&1 | tee .irc_fifo
 rm .irc_fifo
